dff8147146d3c377c4c71bee879f84378106e977
adam.klin <adamklin86@gmail.com>
2020-07-05 16:01:46 +0200

    form

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@ const hamburger = document.querySelector('.hamburger');
 const hamburgerClose = document.querySelector('.hamburger-close');
 const navigationMenu = document.querySelector('header nav');
 
-function showHideMenu() {
 
+const form = document.querySelector(".appointment-form");
+const formFields = document.querySelectorAll(".appointment-form > *");
+const messageInfo = document.querySelector('.appointment-message-info');
+
+//hamburger menu
+function showHideMenu() {
   if (navigationMenu.classList.contains('visibility')) {
     console.log('ukryte');
     navigationMenu.classList.toggle('visibility');
@@ -12,9 +17,74 @@ function showHideMenu() {
     console.log('widoczne');
     navigationMenu.classList.toggle('visibility');
     hamburger.classList.add('hidden');
+  }
+}
+hamburger.addEventListener('click', showHideMenu);
+hamburgerClose.addEventListener('click', showHideMenu);
 
+//wyślij dane na serwer 
+const sendDate = (data) => {
+  fetch('https://akademia108.pl/api/ajax/post-appointment.php', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(res => messageInfo.textContent = res.messages[0])
+    .catch(error => messageInfo.textContent = error)
+}
+
+//obsługa formularza
+
+const [name, email, service, phone, date, time, note] = formFields; // pola formularza
+let formErrors = [];
+
+const formValidation = (e) => {
+  e.preventDefault();
+
+  const clearErrorStyles = () => {
+    formErrors.length = 0;
+    for (let i = 0; i < 7; i++) {
+      formFields[i].className = "";
+    }
+  }
+  clearErrorStyles();
+
+  const addError = (pole) => {
+    pole.classList.add('error');
+    formErrors.push(pole.name);
+  }
+
+  if (name.value.trim() === '') addError(name)
+  if (email.value.trim() === '') addError(email)
+  if (service.value.trim() === '') addError(service)
+  if (phone.value.trim() === '') addError(phone)
+  if (date.value.trim() === '') addError(date)
+  if (time.value.trim() === '') addError(time)
+  if (note.value.trim() === '') addError(note)
+
+  if (formErrors.length === 0) {
+    console.log('brak błędów');
+
+    messageInfo.textContent = "Uzupełnij pola: "
+    messageInfo.textContent += formErrors.join(',');
+
+    let data = {
+      name: name.value,
+      email: email.value,
+      service: service.value,
+      phone: phone.value,
+      date: date.value,
+      time: time.value,
+      message: note.value
+    };
+
+    sendDate(data);
+    form.reset();
   }
 }
 
-hamburger.addEventListener('click', showHideMenu);
-hamburgerClose.addEventListener('click', showHideMenu);
+form.addEventListener('submit', formValidation);
